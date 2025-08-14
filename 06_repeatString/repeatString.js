20ae90c05671b1982a56bbe775faa508c0b206d4
Lerudit <rmiazoloh@gmail.com>
2025-08-14 22:40:34 +0200

    my solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num < 0) return "ERROR";
+    let finalString = '';
+    for (let i = 0; i < num; i++){
+        finalString += word;
+    }
+    return finalString;
 };
+
+//console.log(repeatString('hi',4));
 
 // Do not edit below this line
 module.exports = repeatString;
