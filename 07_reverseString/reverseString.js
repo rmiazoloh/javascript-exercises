dbaf043e3f9d80b1c95d8074cac8e0e1dd1373b5
Lerudit <rmiazoloh@gmail.com>
2025-08-14 22:39:52 +0200

    my solution

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(sentence) {
+    return sentence
+    .split("")
+    .reverse()
+    .join("");
 
 };
-
+console.log(reverseString('Hello World'));
 // Do not edit below this line
 module.exports = reverseString;
