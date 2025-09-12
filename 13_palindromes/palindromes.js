3fcb3ea62ea9fa0adafe2f6024f9c404878ec10d
Lerudit <rmiazoloh@gmail.com>
2025-09-12 21:59:17 +0200

    Solution code for Palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Only alphanumeric characters
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const cleanedString = str
+        .toLowerCase()
+        .split('')
+        .filter((char) => alphanumerical.includes(char))
+        .join('');
+
+    const reversedString = cleanedString.split('').reverse().join('');
+    return cleanedString === reversedString;
 
 };
 
+palindromes('racecar!');
 // Do not edit below this line
 module.exports = palindromes;
