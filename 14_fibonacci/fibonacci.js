fa4df4fc9d1d68e4e84fbd1df7f50b0cd17241aa
Lerudit <rmiazoloh@gmail.com>
2025-09-15 21:09:58 +0200

    Improve perf

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const createFibonnaci = function(r){
+    // Create fibonnaci suite table
+    let arr = [];
+    for (let i = 2; i <= r; i++){
+        arr.push(arr[i-1]+arr[i-2]);
+    }
+    return arr;
+}
+const fibonacci = function(r) {
+    if (r < 0){
+        return 'OOPS';
+    }else {
+        if (isNaN(r)) r = Number(r);
+    }
+    let newArr = createFibonnaci(r);
+    return newArr[r];
 };
 
+fibonacci(4);
 // Do not edit below this line
 module.exports = fibonacci;
