c56f19f234a76267017904f2b09812e10beca6a6
Lerudit <rmiazoloh@gmail.com>
2025-09-16 22:50:16 +0200

    find the oldest man solution

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentAge = 0;
+    let refAge = 0;
 
+    older = people.reduce(function(a, b){
+        b.yearOfDeath = b.yearOfDeath !== undefined ? b.yearOfDeath : new Date().getFullYear();
+        currentAge = b.yearOfDeath - b.yearOfBirth;
+        if (currentAge > refAge){
+            a = { ...b };
+            refAge = a.yearOfDeath - a.yearOfBirth;
+        }
+        return a;
+    },{})
+    return older;
 };
 
 // Do not edit below this line
